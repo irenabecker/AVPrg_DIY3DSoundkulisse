0066a2f24afd083f46a9132c473854dfc2be9e47
Jordi-PC\Jordi <jordanis.lazaridis@haw-hamburg.de>
2018-01-04 14:55:44 +0100

    - add a new Search Form - now you can search between different figures or swap to the left or right. - add a external link to the git and to the members of the project.

---
+++ Front-End/bootstrap-slide-panel/js/searchFigures.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#search').keyup(function() {
+
+        // Search text
+        var text = $(this).val().toLowerCase();
+        // Hide all content class element
+        $('.content').hide();
+
+        // Search 
+        $('.content .figures').each(function() {
+
+            if ($(this).text().toLowerCase().indexOf("" + text + "") != -1) {
+                $(this).closest('.content').show();
+            }
+        });
+    });
+
+    /* Deaktivert das Enter Drücken auf der Webseite, welches dazu führt das die Seite neugeladen wird */
+    
+    $('form input').on('keypress', function(e) {
+        return e.which !== 13;
+    });
+
+});
+
