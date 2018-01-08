67b03cc014701ff08e7e382dffa5bcc943357ac7
Jordi <jordanis.lazaridis@haw-hamburg.de>
2018-01-08 20:02:17 +0100

    - Fixed arrow Bug - added a Carousel Slideshow for the Background - Edit the introduction page - added a Dropdown menu - added a Impressum Side

---
+++ Front-End/background-carousel/background-carousel.php
@@ -0,0 +1,8 @@
+<div id="background-carousel">
+    <div id="myCarousel2" class="carousel slide" data-ride="carousel" data-interval="3000">
+      <div class="carousel-inner">
+        <div class="item active" style="background-image:url(background-carousel/images/lion-wild-africa-african.jpg)"></div>
+        <div class="item" style="background-image:url(http://placehold.it/1600x800/)"></div>  
+      </div>
+    </div>
+</div>
