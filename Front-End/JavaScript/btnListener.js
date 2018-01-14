7b9eb963bdd63e46562840baa5d2dce0ff27f031
CarinaKr <carina.krafft@t-online.de>
2018-01-14 15:31:44 +0100

    change values of default sound objects

--- Front-End/JavaScript/btnListener.js
+++ Front-End/JavaScript/btnListener.js
@@ -12,8 +12,8 @@ function saveDefaultCard(cardIndex)
 {
     let defaultCard=slideDownCards[cardIndex].defaultSoundObject;
     defaultCard.soundFileName=slideDownCards[cardIndex].soundclipName;
-    defaultCard.pitch=slideDownCards[cardIndex].pitch;
-    defaultCard.volume=slideDownCards[cardIndex].soundvolume;
+    defaultCard.pitch=document.getElementById(slideDownCards[cardIndex].pitch).value;
+    defaultCard.volume=document.getElementById(slideDownCards[cardIndex].soundvolume).value;
     
     console.log(defaultSoundObjects);
 }
