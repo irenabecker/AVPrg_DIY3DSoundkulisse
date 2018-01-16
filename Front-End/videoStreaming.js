ef9628051f7d671c8aa58a2f083f432c327a39e8
Jordi <jordanis.lazaridis@haw-hamburg.de>
2018-01-16 01:34:05 +0100

    Enable Camera function

--- Front-End/videoStreaming.js
+++ Front-End/videoStreaming.js
@@ -1,15 +1,30 @@
-var video = document.querySelector("#videoElement");
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({ video: true }, handleVideo, videoError);
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
 }
 
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
+/* Legacy code below: getUserMedia 
+else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }, errBack);
 }
-
-function videoError(e) {
-    // do something
-}
+*/
