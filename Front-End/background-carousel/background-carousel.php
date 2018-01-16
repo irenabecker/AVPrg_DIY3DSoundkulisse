8f56cc848cf789fad00d82529b69547858049c5f
Jordi <jordanis.lazaridis@haw-hamburg.de>
2018-01-16 11:07:30 +0100

    add a new trello image to the Background

--- Front-End/background-carousel/background-carousel.php
+++ Front-End/background-carousel/background-carousel.php
@@ -5,6 +5,7 @@
         <div class="item" style="background-image:url(background-carousel/images/2.png)"></div>  
         <div class="item" style="background-image:url(background-carousel/images/3.png)"></div>  
         <div class="item" style="background-image:url(background-carousel/images/4.png)"></div>  
+        <div class="item" style="background-image:url(background-carousel/images/5.png)"></div>  
       </div>
     </div>
 </div>
