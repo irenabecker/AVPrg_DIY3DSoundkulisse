db5a17e63d7386c10a415397151bb1523e95c66f
CarinaKr <carina.krafft@t-online.de>
2018-01-22 16:38:04 +0100

    typo in sounds

--- Front-End/JavaScript/webAudioScript.js
+++ Front-End/JavaScript/webAudioScript.js
@@ -21,19 +21,19 @@ var citySoundTheme= {};
 var SOUNDS_PATH = '../Sounds/';
 var TESTSOUNDS_PATH = SOUNDS_PATH + 'TestSounds/';
 var THEMEIMAGE_PATH = 'resources/';
-var THEMEPATHS = ['Forest/', 'Mountains/', 'Sea/', 'Urban/', 'Thunder'];
+var THEMEPATHS = ['Forest/', 'Mountains/', 'Sea/', 'Urban/', 'Thunder/'];
 var CURRENT_THEME_PATH = 'Forest/';
 
-var colorValues=[6,1,4];
+var colorValues=[7,3,5];
 var colors = ['Red','Green','Blue'];
 var shapes = ['Rectangle', 'Circle', 'Triangle'];
 
 var themeTest = [
-    ['Birds_And_Slight_Rain.mp3', 'Blowing_Wind.mp3', 'Chimpanzee.mp3', 'Cricket.mp3', 'Frogs.mp3', 'Jungle.mp3', 'Osprey_Call.mp3', 'Raven.mp3', 'Redstart.mp3','Owl.mp3'],   //Forest
-    ['Birds.mp3', 'Blowing_Wind.mp3', 'Yodelling.mp3', 'Raven.mp3', 'Redstart.mp3', 'River.mp3', 'Cow_with_Bell.mp3', 'Goat.mp3', 'Hawk_Scream.mp3', 'Walking_in_Snow.mp3', 'Osprey_Call.mp3'], //Mountains
+    ['Birds_And_Slight_Rain.mp3', 'Blowing_Wind.mp3', 'Chimpanzee.mp3', 'Crickets.mp3', 'Frogs.mp3', 'Jungle.mp3', 'Osprey_Call.mp3', 'Raven.mp3', 'Redstart.mp3','Owl.mp3'],   //Forest
+    ['Birds.mp3', 'Blowing_Wind.mp3', /*'Yodelling.mp3', */'Raven.mp3', 'Redstart.mp3', 'River.mp3', /*'Cow_with_Bell.mp3', 'Goat.mp3', 'Hawk_Scream.mp3', 'Walking_in_Snow.mp3', */'Osprey_Call.mp3'], //Mountains
     ['Blowing_Wind.mp3', 'Calm_Waves.mp3', 'Ducks.mp3', 'Laughing_Gulls_2.mp3', 'Ocean_Waves.mp3', 'Seagull_Single.mp3', 'Seagulls_Swarm.mp3'],   //Sea
     ['Airplane_Takeoff.mp3', 'Car.mp3', 'People_in_Bus.mp3', 'People_in_Mall.mp3', 'Police_Sirens.mp3', 'Traffic_Jam.mp3', 'Train.mp3', 'Urban_Traffic.mp3', 'Hare_Crishna_Chanting.mp3'],   //Urban
-    ['Rainstom.mp3', 'Thunderstorm_and_Rain.mp3', 'Wind.mp3']    //Thunder
+    ['Rainstorm.mp3', 'Thunderstorm_and_Rain.mp3', 'Wind.mp3']    //Thunder
 ];
 
 var themeImages = ['ForestTransparent.png', 'MountainTransparent.png', 'SeaTransparent.png', 'UrbanTransparent.png', 'ThunderTransparent.png'];
